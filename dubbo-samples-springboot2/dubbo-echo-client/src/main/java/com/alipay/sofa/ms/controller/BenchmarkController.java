34ee651adc5e7a0be6141a555c7052f6cede867d
zonghaishang <yiji@apache.org>
2020-04-01 23:13:44 +0800

    benchmark case.

--- dubbo-samples-springboot2/dubbo-echo-client/src/main/java/com/alipay/sofa/ms/controller/BenchmarkController.java
+++ dubbo-samples-springboot2/dubbo-echo-client/src/main/java/com/alipay/sofa/ms/controller/BenchmarkController.java
@@ -23,27 +23,41 @@ public class BenchmarkController {
 
     @GetMapping("/send_512_byte")
     public String send_512_byte() {
-        return benchmarkService.send_512_byte(RandomStringUtils.randomAlphanumeric(512));
+        return benchmarkService.send_512_byte(randomString(512));
     }
 
     @GetMapping("/send_1k")
     public String send_1k() {
-        return benchmarkService.send_1k(RandomStringUtils.randomAlphanumeric(1024));
+        return benchmarkService.send_1k(randomString(1024));
     }
 
     @GetMapping("/send_2k")
     public String send_2k() {
-        return benchmarkService.send_512_byte(RandomStringUtils.randomAlphanumeric(1024 * 2));
+        return benchmarkService.send_512_byte(randomString(1024 * 2));
     }
 
     @GetMapping("/send_4k")
     public String send_4k() {
-        return benchmarkService.send_512_byte(RandomStringUtils.randomAlphanumeric(1024 * 4));
+        return benchmarkService.send_512_byte(randomString(1024 * 4));
     }
 
     @GetMapping("/send_8k")
     public String send_8k() {
-        return benchmarkService.send_512_byte(RandomStringUtils.randomAlphanumeric(1024 * 8));
+        return benchmarkService.send_512_byte(randomString(1024 * 8));
+    }
+
+    static char[] chars = new char[] {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z',
+    };
+
+    static String randomString(int len) {
+        return RandomStringUtils.random(len, 0, chars.length, true, true, chars);
     }
 
 }
