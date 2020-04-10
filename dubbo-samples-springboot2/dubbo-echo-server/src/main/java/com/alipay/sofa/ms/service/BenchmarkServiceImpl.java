5709c48d682d0a855fdc2eb32652464c33bac9b4
zonghaishang <yiji@apache.org>
2020-04-10 14:17:22 +0800

    add logs.

--- dubbo-samples-springboot2/dubbo-echo-server/src/main/java/com/alipay/sofa/ms/service/BenchmarkServiceImpl.java
+++ dubbo-samples-springboot2/dubbo-echo-server/src/main/java/com/alipay/sofa/ms/service/BenchmarkServiceImpl.java
@@ -4,34 +4,53 @@
  */
 package com.alipay.sofa.ms.service;
 
+import com.alibaba.dubbo.rpc.RpcContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author yiji
  * @version : BenchmarkService.java, v 0.1 2020年04月01日 4:47 下午 yiji Exp $
  */
 public class BenchmarkServiceImpl implements BenchmarkService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BenchmarkServiceImpl.class);
+
     @Override
     public String send_512_byte(String content) {
+        String now = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        LOGGER.info("[" + now + "] send_512_byte, request from consumer: " + RpcContext.getContext().getRemoteAddress());
         return content;
     }
 
     @Override
     public String send_1k(String content) {
+        String now = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        LOGGER.info("[" + now + "] send_1k, request from consumer: " + RpcContext.getContext().getRemoteAddress());
         return content;
     }
 
     @Override
     public String send_2k(String content) {
+        String now = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        LOGGER.info("[" + now + "] send_2k, request from consumer: " + RpcContext.getContext().getRemoteAddress());
         return content;
     }
 
     @Override
     public String send_4k(String content) {
+        String now = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        LOGGER.info("[" + now + "] send_4k, request from consumer: " + RpcContext.getContext().getRemoteAddress());
         return content;
     }
 
     @Override
     public String send_8k(String content) {
+        String now = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        LOGGER.info("[" + now + "] send_8k, request from consumer: " + RpcContext.getContext().getRemoteAddress());
         return content;
     }
 
