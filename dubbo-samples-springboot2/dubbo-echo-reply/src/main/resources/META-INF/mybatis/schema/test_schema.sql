359984693b409c5e56050fd227f50e9fd62402ca
zonghaishang <yiji@apache.org>
2020-08-24 15:03:35 +0800

    add schema.

---
+++ dubbo-samples-springboot2/dubbo-echo-reply/src/main/resources/META-INF/mybatis/schema/test_schema.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA `test_sys_info` ;
+
+CREATE TABLE `jdp_tb_trade` (
+  `tid` bigint(20) NOT NULL,
+  `status` varchar(64) NOT NULL,
+  `type` varchar(64) DEFAULT NULL,
+  `seller_nick` varchar(32) DEFAULT NULL,
+  `buyer_nick` varchar(256) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `jdp_hashcode` varchar(128) NOT NULL,
+  `jdp_response` mediumtext,
+  `jdp_created` datetime NOT NULL,
+  `jdp_modified` datetime NOT NULL,
+  PRIMARY KEY (`tid`),
+  KEY `seller_jdpmodified` (`seller_nick`,`jdp_modified`),
+  KEY `jdpmodified` (`jdp_modified`),
+  KEY `seller_modified` (`seller_nick`,`modified`),
+  KEY `modified` (`modified`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单推送库'
