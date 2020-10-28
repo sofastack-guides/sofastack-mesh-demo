1dd5bc4c02a345d947e8b0bdbc8e218e01e1ae47
xingqi.xq <xingqi.xq@alibaba-inc.com>
2020-10-28 13:41:18 +0800

    mvn clean

--- springcloud-samples-springboot2/springcloud-reservation-service/start.sh
+++ springcloud-samples-springboot2/springcloud-reservation-service/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-java  -Dmesh_http_read_timeout=600000 -Duser.timezone=Asia/Shanghai -jar /home/admin/release/springcloud-reservation-service-1.0-SNAPSHOT.jar
+java ${JAVA_OPT} -Duser.timezone=Asia/Shanghai -jar /home/admin/release/springcloud-reservation-service-1.0-SNAPSHOT.jar
