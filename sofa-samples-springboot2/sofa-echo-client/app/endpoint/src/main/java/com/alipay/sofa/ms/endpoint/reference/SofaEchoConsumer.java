9221955b650814f82bb60caafde2dd49f5c628a0
zonghaishang <yiji@apache.org>
2021-03-03 17:32:03 +0800

    bolt wasm demo

--- sofa-samples-springboot2/sofa-echo-client/app/endpoint/src/main/java/com/alipay/sofa/ms/endpoint/reference/SofaEchoConsumer.java
+++ sofa-samples-springboot2/sofa-echo-client/app/endpoint/src/main/java/com/alipay/sofa/ms/endpoint/reference/SofaEchoConsumer.java
@@ -27,17 +27,32 @@ public class SofaEchoConsumer implements ApplicationContextAware {
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         SofaEchoService echoService = (SofaEchoService) applicationContext.getBean("echoService"); // get remote service proxy
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        new Thread(() -> {
-            for (; ; ) {
-                try {
-                    TimeUnit.SECONDS.sleep(1L);
-                    String status1 = echoService.echo("Hello world!");
-                    System.out.println(">>>>>>>> " + fmt.format(new Date())  + " echo result: " + status1);
-                    logger.info(">>>>>>>> echo result: " + status1);
-                } catch (Exception e) {
-                    logger.error(">>>>>>>> echo result: " + e.getMessage());
+        for (int i = 0; i < 1; i++) {
+            new Thread(() -> {
+                int count = 1;
+                for (; ; ) {
+                    try {
+                        //if (count++ <=1) {
+                            long start = System.currentTimeMillis();
+                            String status1 = echoService.echo("Hello world!");
+                            long elapsed = System.currentTimeMillis() -start;
+                            Thread thread = Thread.currentThread();
+                            System.out.println(
+                                    ">>>>>>>> [" + thread.getId() + "," + (count++) + "," + elapsed + "ms]" + fmt.format(new Date()) + " echo result: " + status1);
+                            logger.info(">>>>>>>> echo result: " + status1);
+                        //}
+                        TimeUnit.SECONDS.sleep(1L);
+                    } catch (Exception e) {
+                        try {
+                            TimeUnit.SECONDS.sleep(1L);
+                        } catch (InterruptedException interruptedException) {
+                            interruptedException.printStackTrace();
+                        }
+                        System.err.println(">>>>>>>> failed: " + e.getMessage());
+                        logger.error(">>>>>>>> echo result: " + e.getMessage());
+                    }
                 }
-            }
-        }).start();
+            }).start();
+        }
     }
 }
--- sofa-samples-springboot2/sofa-echo-server/app/endpoint/src/main/java/com/alipay/sofa/ms/endpoint/impl/SofaEchoServiceImpl.java
+++ sofa-samples-springboot2/sofa-echo-server/app/endpoint/src/main/java/com/alipay/sofa/ms/endpoint/impl/SofaEchoServiceImpl.java
@@ -6,6 +6,9 @@ package com.alipay.sofa.ms.endpoint.impl;
 
 import com.alipay.sofa.ms.service.SofaEchoService;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author yiji
  * @version : SofaEchoServiceImpl.java, v 0.1 2020年04月21日 1:50 下午 yiji Exp $
@@ -13,7 +16,9 @@ import com.alipay.sofa.ms.service.SofaEchoService;
 public class SofaEchoServiceImpl implements SofaEchoService {
     @Override
     public String echo(String message) {
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         // we don't care about message content.
+        System.out.println(">>>>>>>> " + fmt.format(new Date())  + " message: " + message);
         return "success";
     }
 }
