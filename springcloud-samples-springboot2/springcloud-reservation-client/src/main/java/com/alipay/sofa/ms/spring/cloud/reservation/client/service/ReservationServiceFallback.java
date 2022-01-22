49821976dd50157bcd39c8f41246191e262e5108
YIDWang <1520990951@qq.com>
2022-01-22 17:10:06 +0800

    Update ReservationServiceFallback.java

--- springcloud-samples-springboot2/springcloud-reservation-client/src/main/java/com/alipay/sofa/ms/spring/cloud/reservation/client/service/ReservationServiceFallback.java
+++ springcloud-samples-springboot2/springcloud-reservation-client/src/main/java/com/alipay/sofa/ms/spring/cloud/reservation/client/service/ReservationServiceFallback.java
@@ -12,4 +12,5 @@ public class ReservationServiceFallback implements ReservationService {
   public Resources<Reservation> queryReservations() {
     return new Resources<>(Collections.emptyList());
   }
+  
 }
