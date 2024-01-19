5e4508b032fbaa13a0021faf4b45cf726643d655
zonghai.szh <zonghai.szh@antgroup.com>
2024-01-19 14:53:26 +0800

    1.6.4-dubbo3

--- dubbo-samples-springboot2/dubbo-echo-client/src/main/java/com/alipay/sofa/ms/AdaptiveController.java
+++ dubbo-samples-springboot2/dubbo-echo-client/src/main/java/com/alipay/sofa/ms/AdaptiveController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class AdaptiveController {
 
 
-    @DubboReference(retries = 0)
+    @DubboReference(retries = 0, check = false)
     private InvokeService invokeService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(com.alipay.sofa.ms.AdaptiveController.class);
